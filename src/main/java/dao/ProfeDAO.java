1d208c0a20f3c3eaedca5d68216deb87eabcb59c
Miguel <migueldiaz.tg@gmail.com>
2018-01-09 11:15:48 +0100

    Corregida query para coger todas las notas de un profesor

--- src/main/java/dao/ProfeDAO.java
+++ src/main/java/dao/ProfeDAO.java
@@ -6,7 +6,10 @@
 package dao;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.Nota;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -20,7 +23,9 @@ public class ProfeDAO {
             + "FROM users u JOIN alumnos_asignaturas aa ON u.id = aa.id_alumno"
             + "JOIN asignaturas a ON a.id = aa.id_asignatura"
             + "JOIN profesores_asignaturas pa ON pa.id_asignatura = aa.id_asignatura"
-            + "WHERE pa.id_profesor = ?";
+            + "WHERE pa.id_profesor = ?"
+            + "ORDER BY a.id";
+    private final String queryGetId = "SELECT id FROM users WHERE email = ?";
     
     public List<Nota> getAllNotas() {
         JdbcTemplate jtm = new JdbcTemplate(DBConnection.getInstance().getDataSource());
@@ -28,4 +33,16 @@ public class ProfeDAO {
 
         return notas;
     }
+    
+    public int getId(String email){
+        int id = 0;
+        try {
+            JdbcTemplate jtm = new JdbcTemplate(DBConnection.getInstance().getDataSource());
+            id = jtm.queryForObject(queryGetId, int.class, email);
+
+        } catch (DataAccessException ex) {
+            Logger.getLogger(UsersDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return id;
+    } 
 }
