533b7d2105fc066520a09636fbdfdc5cf608913a
PORTATIL <andreigarciacuadra@gmail.com>
2018-01-11 13:10:22 +0100

    fix para querys

--- src/main/java/dao/ProfeDAO.java
+++ src/main/java/dao/ProfeDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao;
 
 import java.util.List;
@@ -13,24 +8,20 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-/**
- *
- * @author Miguel
- */
 public class ProfeDAO {
     
     private final String queryGetAllNotas = "SELECT u.id, u.nombre, a.id AS id_asignatura, a.nombre AS nombre_asignatura, aa.nota "
-            + "FROM users u JOIN alumnos_asignaturas aa ON u.id = aa.id_alumno"
-            + "JOIN asignaturas a ON a.id = aa.id_asignatura"
-            + "JOIN profesores_asignaturas pa ON pa.id_asignatura = aa.id_asignatura"
-            + "WHERE pa.id_profesor = ?"
+            + "FROM users u JOIN alumnos_asignaturas aa ON u.id = aa.id_alumno "
+            + "JOIN asignaturas a ON a.id = aa.id_asignatura "
+            + "JOIN profesores_asignaturas pa ON pa.id_asignatura = aa.id_asignatura "
+            + "WHERE pa.id_profesor = ? "
             + "ORDER BY a.id";
     private final String queryGetAllNotasCursos = "SELECT c.id AS id_curso, c.nombre AS nombre_curso, u.id, u.nombre, a.id AS id_asignatura, a.nombre AS nombre_asignatura, aa.nota "
-            + "FROM users u JOIN alumnos_asignaturas aa ON u.id = aa.id_alumno"
-            + "JOIN asignaturas a ON a.id = aa.id_asignatura"
-            + "JOIN profesores_asignaturas pa ON pa.id_asignatura = aa.id_asignatura"
-            + "JOIN cursos c ON c.id = a.id_curso"
-            + "WHERE pa.id_profesor = ?"
+            + "FROM users u JOIN alumnos_asignaturas aa ON u.id = aa.id_alumno "
+            + "JOIN asignaturas a ON a.id = aa.id_asignatura "
+            + "JOIN profesores_asignaturas pa ON pa.id_asignatura = aa.id_asignatura "
+            + "JOIN cursos c ON c.id = a.id_curso "
+            + "WHERE pa.id_profesor = ? "
             + "ORDER BY c.id";
     private final String queryGetId = "SELECT id FROM users WHERE email = ?";
     private final String queryModNota = "UPDATE alumnos_asignaturas SET nota = ? WHERE id_alumno = ? AND id_asignatura = ?";
